d26ada6018ccfd5c328f7220e0b0e1c78b29bf06
Nicotorz <nico_geve_2000@hotmail.es>
2025-01-21 20:22:45 -0300

    Pantalla de NotFound agregada

--- memotest/src/App.jsx
+++ memotest/src/App.jsx
@@ -3,6 +3,7 @@ import { ConfigProvider } from './hooks/ConfigContext';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Menu from './components/menu/Menu';
 import Board from './components/Board/board';
+import NotFound from './components/menu/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Menu />} />
           <Route path="/Menu" element={<Menu />} />
           <Route path="/Game" element={<Board />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </ConfigProvider>
